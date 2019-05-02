69803a16c93c5a120af8186da901559e9a930c48
Alexandre Demers <alexandre.f.demers@gmail.com>
2019-05-02 01:38:15 -0400

    Add missing header definition for usbip_vhci.h
    Good practice.
    
    Signed-off-by: Alexandre Demers <alexandre.f.demers@gmail.com>

--- userspace/src/usbip/usbip_vhci.h
+++ userspace/src/usbip/usbip_vhci.h
@@ -1,3 +1,6 @@
+#ifndef __VHCI_DRIVER_H
+#define __VHCI_DRIVER_H
+
 #pragma
 
 #include "usbip_wudev.h"
@@ -7,3 +10,5 @@ void usbip_vhci_driver_close(HANDLE hdev);
 int usbip_vhci_get_free_port(HANDLE hdev);
 int usbip_vhci_attach_device(HANDLE hdev, int port, usbip_wudev_t *wudev);
 int usbip_vhci_detach_device(HANDLE hdev, int port);
+
+#endif /* __VHCI_DRIVER_H */
