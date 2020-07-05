19709d3a377f3f32b81ce798c56ca54ada04cba6
KyungWoon Cho <cezanne@codemayo.com>
2020-07-05 20:18:38 +0900

    bug fix for vhci(ude) control transfer
    There was a bug that fetching from OUT control transfer tried to copy
    from a transfer buffer due to invalid transfer direction logic.

--- driver/vhci_ude/vhci_urbr_fetch_control.c
+++ driver/vhci_ude/vhci_urbr_fetch_control.c
@@ -9,7 +9,7 @@ fetch_urbr_control_transfer(PURB urb, struct usbip_header *hdr)
 	struct _URB_CONTROL_TRANSFER	*urb_ctltrans = &urb->UrbControlTransfer;
 	NTSTATUS	status;
 
-	if (urb_ctltrans->TransferBufferLength == 0)
+	if (!IS_TRANSFER_FLAGS_IN(urb_ctltrans->TransferFlags))
 		return STATUS_SUCCESS;
 	status = copy_to_transfer_buffer(urb_ctltrans->TransferBuffer, urb_ctltrans->TransferBufferMDL,
 		urb_ctltrans->TransferBufferLength, hdr + 1, hdr->u.ret_submit.actual_length);
@@ -24,6 +24,8 @@ fetch_urbr_control_transfer_ex(PURB urb, struct usbip_header *hdr)
 	struct _URB_CONTROL_TRANSFER_EX	*urb_ctltrans_ex = &urb->UrbControlTransferEx;
 	NTSTATUS	status;
 
+	if (!IS_TRANSFER_FLAGS_IN(urb_ctltrans_ex->TransferFlags))
+		return STATUS_SUCCESS;
 	status = copy_to_transfer_buffer(urb_ctltrans_ex->TransferBuffer, urb_ctltrans_ex->TransferBufferMDL,
 		urb_ctltrans_ex->TransferBufferLength, hdr + 1, hdr->u.ret_submit.actual_length);
 	if (status == STATUS_SUCCESS)
