8409c83faec6c19fb5d021f81708eaef0f80dd3c
KyungWoon Cho <cezanne@codemayo.com>
2021-01-13 13:31:34 +0900

    Present a usbip-win version for usbip tools

--- userspace/config.h
+++ userspace/config.h
@@ -2,31 +2,10 @@
 #ifndef _USBIP_CONFIG_WINDOWS_H
 #define _USBIP_CONFIG_WINDOWS_H
 
-/* Name of package */
-#define PACKAGE "usbip"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "usbip-devel@lists.sourceforge.net"
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "usbip"
-
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "usbip 1.0.0"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "usbip"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.0"
+#define PACKAGE_STRING "usbip-win 0.3.2"
 
 /* numeric version number */
 #define USBIP_VERSION 0x000111
-
-/* Version number of package */
-#define VERSION "1.0.0"
 
 #endif
